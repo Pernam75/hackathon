718c5a12689a523e15320b60319bd1f38c9309ad
Pernam75 <jules.rubin@efrei.net>
2023-06-13 13:56:02 +0200

    hackathon

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,158 +1,236 @@
-import altair as alt
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 import streamlit as st
-from vega_datasets import data
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from PIL import Image
 
-st.set_page_config(
-    page_title="Time series annotations", page_icon="⬇", layout="centered"
-)
+# Load data
+df = pd.read_csv('df.csv')
+country_list = df['Country'].unique().tolist()
+country_list_without_france = country_list.copy()
+country_list_without_france.remove('France')
+
+# Function to display data
+def display_data():
+    st.write(df.head(100))
+
+# Function to display descriptive statistics
+def display_statistics():
+    st.write(df.describe())
+    # Plot the co2 emission of the countries of the df on a map for 2000
+    fig = px.choropleth(df[df['Year'] == 2000], locations="Country", locationmode='country names', color="CO2e_Emissions", hover_name="Country", color_continuous_scale=px.colors.sequential.Plasma, title='CO2 emission of the countries of the df on a map for 2000')
+    st.plotly_chart(fig)
+
+    # Plot the co2 emission of the countries of the df on a map for 2021
+    fig = px.choropleth(df[df['Year'] == 2021], locations="Country", locationmode='country names', color="CO2e_Emissions", hover_name="Country", color_continuous_scale=px.colors.sequential.Plasma, title='CO2 emission of the countries of the df on a map for 2021')
+    st.plotly_chart(fig)
+
+def analysis_emissions_consumption():
+    options = st.multiselect(
+    'Choose the country of analysis:',
+    country_list_without_france)
+
+    st.write('Choose the country of analysis:', options)
+
+    choose_country = ['France']
+    if options != []:
+        choose_country = choose_country + options
+
+    df_choose = df[df['Country'].isin(choose_country)]
+    st.write(df_choose.head())
+
+    fig = plt.figure(figsize=(20, 10))
+    fig = px.line(df_choose, x="Year", y="CO2e_Emissions", color='Country')
+    st.plotly_chart(fig)
+
+    energy_sources = ['Oil Consumption - EJ', 'Gas Consumption - EJ',
+                  'Coal Consumption - EJ', 'Nuclear Consumption - EJ',
+                  'Hydro Consumption - EJ', 'Solar Consumption - EJ',
+                  'Wind Consumption - EJ', 'Geo Biomass Other - EJ',
+                  'Biofuels consumption - EJ']
+
+    df_choose_2000 = df_choose[df_choose['Year'] == 2000]
+    df_choose_2000[energy_sources] = df_choose_2000[energy_sources].div(df_choose_2000[energy_sources].sum(axis=1), axis=0)
+    energy_by_country = df_choose_2000.pivot_table(values=energy_sources, index='Country')
+
+    fig = plt.figure(figsize=(12, 8))
+    sns.heatmap(energy_by_country, cmap='YlGnBu')
+    plt.xlabel('Energy Sources')
+    plt.ylabel('Country')
+    plt.title('Energy Consumption by Country and Source - 2000')
+    st.pyplot(fig)
+
+    df_choose_2021 = df_choose[df_choose['Year'] == 2021]
+    df_choose_2021[energy_sources] = df_choose_2021[energy_sources].div(df_choose_2021[energy_sources].sum(axis=1), axis=0)
+    energy_by_country = df_choose_2021.pivot_table(values=energy_sources, index='Country')
+
+    fig = plt.figure(figsize=(12, 8))
+    sns.heatmap(energy_by_country, cmap='YlGnBu')
+    plt.xlabel('Energy Sources')
+    plt.ylabel('Country')
+    plt.title('Energy Consumption by Country and Source - 2021')
+    st.pyplot(fig)
+
+    # Plot the evolution of the energy consumption in a subplots per country
+    fig, axes = plt.subplots(3, 3, figsize=(20, 10))
+    fig.suptitle('Energy Consumption by Country and Source - 2000')
+    sns.barplot(ax=axes[0, 0], x='Country', y='Oil Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[0, 1], x='Country', y='Gas Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[1, 0], x='Country', y='Coal Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[1, 1], x='Country', y='Nuclear Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[2, 0], x='Country', y='Hydro Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[2, 1], x='Country', y='Solar Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[0, 2], x='Country', y='Wind Consumption - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[1, 2], x='Country', y='Geo Biomass Other - EJ', data=df_choose_2000)
+    sns.barplot(ax=axes[2, 2], x='Country', y='Biofuels consumption - EJ', data=df_choose_2000)
+    st.pyplot(fig)
+
+    # Plot the evolution of the energy consumption in a subplots per country
+    fig, axes = plt.subplots(3, 3, figsize=(20, 10))
+    fig.suptitle('Energy Consumption by Country and Source - 2021')
+    sns.barplot(ax=axes[0, 0], x='Country', y='Oil Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[0, 1], x='Country', y='Gas Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[1, 0], x='Country', y='Coal Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[1, 1], x='Country', y='Nuclear Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[2, 0], x='Country', y='Hydro Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[2, 1], x='Country', y='Solar Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[0, 2], x='Country', y='Wind Consumption - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[1, 2], x='Country', y='Geo Biomass Other - EJ', data=df_choose_2021)
+    sns.barplot(ax=axes[2, 2], x='Country', y='Biofuels consumption - EJ', data=df_choose_2021)
+    st.pyplot(fig)
+
+def prediction_co2_emissions_2022_world():
+    df_predict = pd.DataFrame(columns=['Country', 'CO2e_Emissions','Pourcentage'])
+    list_country = df["Country"].unique()
+    for country in list_country:
+        df_count = df[df['Country'] == country]
+        df_count = df_count.drop(columns=['Country'])
+        X = df_count[['Year', 'Oil Consumption - EJ', 'Gas Consumption - EJ',
+        'Coal Consumption - EJ', 'Nuclear Consumption - EJ',
+        'Hydro Consumption - EJ', 'Solar Consumption - EJ',
+        'Wind Consumption - EJ', 'Geo Biomass Other - EJ',
+        'Biofuels consumption - EJ']]
+        y = df_count['CO2e_Emissions']
+        X_train = X[X['Year'] < 2019]
+        X_test = X[X['Year'] >= 2019]
+        y_train = y[X['Year'] < 2019]
+        y_test = y[X['Year'] >= 2019]
+
+        linreg = LinearRegression()
+        linreg.fit(X_train, y_train)
+        y_pred = linreg.predict(X_test)
+        X_2022 = df_count[df_count['Year'] == 2021][['Year', 'Oil Consumption - EJ', 'Gas Consumption - EJ',
+                                        'Coal Consumption - EJ', 'Nuclear Consumption - EJ',
+                                        'Hydro Consumption - EJ', 'Solar Consumption - EJ', 
+                                        'Wind Consumption - EJ', 'Geo Biomass Other - EJ',
+                                        'Biofuels consumption - EJ']]
+        y_2022 = linreg.predict(X_2022)
+        val = df_count[df_count['Year'] == 2021]['CO2e_Emissions'].values
+        pourcent_predict= y_2022[0]/val[0]
+        df_predict = df_predict.append({'Country': country, 'CO2e_Emissions': y_2022[0], 'Pourcentage': pourcent_predict}, ignore_index=True)
+
+    df_2022_pourcent = df_predict.copy()
+    df_2022_pourcent["Pourcentage"] = df_2022_pourcent["Pourcentage"] -1
+    df_2022_pourcent["Pourcentage"] = df_2022_pourcent["Pourcentage"] * 100
+    df_2022_pourcent.drop(columns=['CO2e_Emissions'], inplace=True)
+    df_2022_pourcent = df_2022_pourcent[df_2022_pourcent['Country'] != 'Vietnam']
+    df_2022_pourcent = df_2022_pourcent[df_2022_pourcent['Country'] != 'Colombia']
+
+    df_2022_emission = df_predict.copy()
+    df_2022_emission.drop(columns=['Pourcentage'], inplace=True)
+    df_2022_emission = df_2022_emission[df_2022_emission['Country'] != 'Vietnam']
+    df_2022_emission = df_2022_emission[df_2022_emission['Country'] != 'Colombia']
 
 
-@st.experimental_memo
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
+    fig = px.choropleth(df_2022_emission, locations="Country", locationmode='country names', color="CO2e_Emissions", hover_name="Country", color_continuous_scale=px.colors.sequential.Plasma, title='CO2 emission of the countries on a map for 2022')
+    st.plotly_chart(fig)
+
+    # Plot the pourcentage of CO2 emission for 2022 on a map
+    fig = px.choropleth(df_2022_pourcent, locations="Country", locationmode='country names', color="Pourcentage", hover_name="Country", color_continuous_scale=px.colors.sequential.Plasma, title='Pourcentage of CO2 emission for 2022')
+    st.plotly_chart(fig)
+
+def prediction_co2_emissions_2022_country():
+    option = st.selectbox('Select the country where you want the prediction',country_list)
+    print(option)
+    if option:
+        df_fr = df[df['Country'] == option]
+        print(df_fr)
+        X = df_fr[['Year', 'Oil Consumption - EJ', 'Gas Consumption - EJ',
+            'Coal Consumption - EJ', 'Nuclear Consumption - EJ',
+            'Hydro Consumption - EJ', 'Solar Consumption - EJ',
+            'Wind Consumption - EJ', 'Geo Biomass Other - EJ',
+            'Biofuels consumption - EJ']]
+        y = df_fr['CO2e_Emissions']
+
+        X_train = X[X['Year'] < 2019]
+        X_test = X[X['Year'] >= 2019]
+        y_train = y[X['Year'] < 2019]
+        y_test = y[X['Year'] >= 2019]
+
+        # Create the model
+        linreg = LinearRegression()
+
+        # Train the model
+        linreg.fit(X_train, y_train)
+
+        # Make predictions
+        y_pred = linreg.predict(X_test)
+
+        # Evaluate the model
+        st.write('Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
+        st.write('Coefficient of determination: %.2f' % r2_score(y_test, y_pred))
+
+        # Predict the CO2 emission for 2022
+        X_2022 = df_fr[df_fr['Year'] == 2021][['Year', 'Oil Consumption - EJ', 'Gas Consumption - EJ',
+                                                'Coal Consumption - EJ', 'Nuclear Consumption - EJ',
+                                                'Hydro Consumption - EJ', 'Solar Consumption - EJ',
+                                                'Wind Consumption - EJ', 'Geo Biomass Other - EJ',
+                                                'Biofuels consumption - EJ']]
+        y_2022 = linreg.predict(X_2022)
+        st.write('Predicted CO2 emission for 2022: %.2f' % y_2022)
+        val = df_fr[df_fr['Year'] == 2021]['CO2e_Emissions'].values
+        pourcent_predict= y_2022[0]/val[0]
+        print(pourcent_predict)
+        st.write('Pourcentage of variation between 2021 and our prediction for 2022: ', pourcent_predict)
+
+        # Plot the evolution of the energy consumption for France with 2022 prediction
+        fig = plt.figure(figsize=(20, 10))
+        fig = px.line(df_fr, x="Year", y="CO2e_Emissions", color='Country')
+        fig.add_scatter(x=[2022], y=y_2022, mode='markers', name='2022 prediction')
+        st.plotly_chart(fig)
+
+# Main function to run the Streamlit app
+def main():    
+    # Sidebar options
+    option = st.sidebar.selectbox('Select an option', ('Display Data', 'Display Statistics', 'Analysis of the emissions of CO2 in the world and consumption of energy', 'Prediction of CO2 emissions in 2022', 'Prediction of CO2 emissions in 2022 for a country', 'Forecasting'))
+    
+    # Display data or statistics based on the selected option
+    if option == 'Display Data':
+        st.title('Display the data')
+        display_data()
+    elif option == 'Display Statistics':
+        st.title('Exploratory Data Analysis')
+        display_statistics()
+    elif option == 'Analysis of the emissions of CO2 in the world and consumption of energy':
+        st.title('Analysis between France and choosen country')
+        analysis_emissions_consumption()
+    elif option == 'Prediction of CO2 emissions in 2022':
+        st.title('Prediction of CO2 emission in 2022')
+        prediction_co2_emissions_2022_world()
+    elif  option == 'Prediction of CO2 emissions in 2022 for a country':
+        st.title('Prediction of CO2 emission in 2022 for a country')
+        prediction_co2_emissions_2022_country()
+    elif  option == 'Forecasting':
+        st.title('Forecasting the CO2 emission for France')
+        image = Image.open('forecasting_france.png')
+        st.image(image, caption='Foercasting the CO2 emission for France')
+        
+        
 
 
-@st.experimental_memo(ttl=60 * 60 * 24)
-def get_chart(data):
-    hover = alt.selection_single(
-        fields=["date"],
-        nearest=True,
-        on="mouseover",
-        empty="none",
-    )
-
-    lines = (
-        alt.Chart(data, title="Evolution of stock prices")
-        .mark_line()
-        .encode(
-            x="date",
-            y="price",
-            color="symbol",
-            # strokeDash="symbol",
-        )
-    )
-
-    # Draw points on the line, and highlight based on selection
-    points = lines.transform_filter(hover).mark_circle(size=65)
-
-    # Draw a rule at the location of the selection
-    tooltips = (
-        alt.Chart(data)
-        .mark_rule()
-        .encode(
-            x="yearmonthdate(date)",
-            y="price",
-            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-            tooltip=[
-                alt.Tooltip("date", title="Date"),
-                alt.Tooltip("price", title="Price (USD)"),
-            ],
-        )
-        .add_selection(hover)
-    )
-
-    return (lines + points + tooltips).interactive()
-
-
-st.title("⬇ Time series annotations")
-
-st.write("Give more context to your time series using annotations!")
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    ticker = st.text_input("Choose a ticker (⬇💬👇ℹ️ ...)", value="⬇")
-with col2:
-    ticker_dx = st.slider(
-        "Horizontal offset", min_value=-30, max_value=30, step=1, value=0
-    )
-with col3:
-    ticker_dy = st.slider(
-        "Vertical offset", min_value=-30, max_value=30, step=1, value=-10
-    )
-
-# Original time series chart. Omitted `get_chart` for clarity
-source = get_data()
-chart = get_chart(source)
-
-# Input annotations
-ANNOTATIONS = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-
-# Create a chart with annotations
-annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-annotations_df.date = pd.to_datetime(annotations_df.date)
-annotations_df["y"] = 0
-annotation_layer = (
-    alt.Chart(annotations_df)
-    .mark_text(size=15, text=ticker, dx=ticker_dx, dy=ticker_dy, align="center")
-    .encode(
-        x="date:T",
-        y=alt.Y("y:Q"),
-        tooltip=["event"],
-    )
-    .interactive()
-)
-
-# Display both charts together
-st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
-
-st.write("## Code")
-
-st.write(
-    "See more in our public [GitHub repository](https://github.com/streamlit/example-app-time-series-annotation)"
-)
-
-st.code(
-    f"""
-import altair as alt
-import pandas as pd
-import streamlit as st
-from vega_datasets import data
-
-@st.experimental_memo
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
-
-source = get_data()
-
-# Original time series chart. Omitted `get_chart` for clarity
-chart = get_chart(source)
-
-# Input annotations
-ANNOTATIONS = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-
-# Create a chart with annotations
-annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-annotations_df.date = pd.to_datetime(annotations_df.date)
-annotations_df["y"] = 0
-annotation_layer = (
-    alt.Chart(annotations_df)
-    .mark_text(size=15, text="{ticker}", dx={ticker_dx}, dy={ticker_dy}, align="center")
-    .encode(
-        x="date:T",
-        y=alt.Y("y:Q"),
-        tooltip=["event"],
-    )
-    .interactive()
-)
-
-# Display both charts together
-st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
-
-""",
-    "python",
-)
+# Run the app
+if __name__ == '__main__':
+    main()
